3922e16f1c7c32d808c2c8810fbc3658c00cc351
CBurton1 <chrislukeburton@gmail.com>
2017-04-07 13:29:08 -0300

    able to get inputs and add to get call

--- frontend/script.js
+++ frontend/script.js
@@ -10,9 +10,20 @@
 
 function getRecipes()
 {
+
+    var likes = $("#likes").tagsinput('items');
+    var dislikes = $("#dislikes").tagsinput('items');
+
+    likes = likes.join();
+    likes.replace(",","%2C+");
+
+    dislikes = dislikes.join();
+    dislikes.replace(",","%2C+");
+
+    var seperator = '%2C+';
     //ajax call to read all events
     //get request with hard-coded query params, replace these with data from the inputs
-        $.get("http://localhost:3000/?dislikes=oranges%2C+olives&likes=pork", function(data, status){
+        $.get("http://localhost:3000/?dislikes=" + dislikes + "&likes=" + likes, function(data, status){
             console.log(data);
             //run through the data to get each individual recipe
             for(var i = 0; i<data.body.results.length; i++){
@@ -28,7 +39,9 @@ function getRecipes()
 
 
 $(document).ready(function(){
-    getRecipes();
+    $('#submit').click(function(){
+        getRecipes();
+    })
 
     $("search").click(function(){
         var likes = $("likes").val();
